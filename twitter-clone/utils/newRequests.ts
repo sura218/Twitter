66a46e1c84183aa38fa0a282289a50657d7c75e6
surafel <surafeldebebe2021@gmail.com>
2026-04-19 12:25:15 +0300

    another test

--- twitter-clone/utils/newRequests.ts
+++ twitter-clone/utils/newRequests.ts
@@ -1,11 +1,9 @@
 import axios from 'axios'
 
 const newRequest = axios.create({
-  baseURL:         process.env.NEXT_PUBLIC_API_URL ?'http://localhost:5500/api': '/api',
+  baseURL:         process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5500/api',
   withCredentials: true,   // ← sends cookie automatically with every request
 })
-
-console.log("NEXT_PUBLIC_API_URL", process.env.NEXT_PUBLIC_API_URL)
 
 // if session expired redirect to login
 newRequest.interceptors.response.use(
